ee362f69f08503a0b4ae211a9ae8d32fdca96362
deepinbubblegum <moondeepin@gmail.com>
2022-09-25 18:09:15 +0700

    add pages and route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/oders', function () {
+    return view('oders');
+});
+
+Route::get('/process', function () {
+    return view('oders');
+});
