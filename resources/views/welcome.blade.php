ae44a68c4fc42cd2fbaf293a581966fc3b8d2927
deepinbubblegum <moondeepin@gmail.com>
2022-09-25 16:44:00 +0700

    add Breadcrumb

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <title>K-WD Dashboard | Mini + One Columns Sidebar</title>
-    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap"
-        rel="stylesheet" />
+    <title>Sterile traceability</title>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}" />
     <script src="{{ asset('assets/component.min.js') }}"></script>
     <script src="{{ asset('assets/alpine.min.js') }}" defer></script>
+    @vite('resources/js/app.js')
 </head>
 
 <body>
@@ -21,22 +21,46 @@
                 class="fixed inset-0 z-50 flex items-center justify-center text-2xl font-semibold text-white bg-primary-darker">
                 Loading.....
             </div>
-
-
             @include('component.slidebar')
-
-
             <!-- Main content -->
             <main class="flex-1">
                 <div class="flex flex-col flex-1 h-full min-h-screen p-4 overflow-x-hidden overflow-y-auto">
-                    @for ($i = 0 ; $i < 1500 ; $i++)
-                        hello
-                        <br>
-                    @endfor
+                    <div class="mx-auto rounded-md bg-white dark:bg-darker dark:text-light p-4 mb-4 leading-6 ">
+                        <nav class="flex" aria-label="Breadcrumb">
+                            <ol class="inline-flex items-center space-x-1 md:space-x-3">
+                              <li class="inline-flex items-center">
+                                <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                                  <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                                  Home
+                                </a>
+                              </li>
+                              <li>
+                                <div class="flex items-center">
+                                  <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                                  <a href="#" class="ml-1 text-sm font-medium text-gray-700 hover:text-gray-900 md:ml-2 dark:text-gray-400 dark:hover:text-white">Projects</a>
+                                </div>
+                              </li>
+                              <li aria-current="page">
+                                <div class="flex items-center">
+                                  <svg class="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                                  <span class="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">Flowbite</span>
+                                </div>
+                              </li>
+                            </ol>
+                          </nav>
+                    </div>
+
+                    <div class="mx-auto h-full rounded-md bg-white dark:bg-darker dark:text-light shadow-sm p-4 leading-6 ">
+
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat perferendis iusto quo quod. Ut deleniti, deserunt quaerat veniam repellendus corrupti rerum doloremque quod libero nulla inventore adipisci et pariatur officia.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat perferendis iusto quo quod. Ut deleniti, deserunt quaerat veniam repellendus corrupti rerum doloremque quod libero nulla inventore adipisci et pariatur officia.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat perferendis iusto quo quod. Ut deleniti, deserunt quaerat veniam repellendus corrupti rerum doloremque quod libero nulla inventore adipisci et pariatur officia.
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat perferendis iusto quo quod. Ut deleniti, deserunt quaerat veniam repellendus corrupti rerum doloremque quod libero nulla inventore adipisci et pariatur officia.
+
+                    </div>
+
                 </div>
             </main>
-
-
         </div>
     </div>
 
