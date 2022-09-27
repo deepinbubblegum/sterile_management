842f060cc2dac334cca9396d62fb6a88df27bee0
deepinbubblegum <moondeepin@gmail.com>
2022-09-28 00:53:38 +0700

    update

--- resources/views/component/Tagheader.blade.php
+++ resources/views/component/Tagheader.blade.php
@@ -2,6 +2,7 @@
 {{-- <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@100;200&display=swap" rel="stylesheet"> --}}
 <link href="https://fonts.googleapis.com/css2?family=Bai+Jamjuree:wght@200;300;400;600;700;900&display=swap" rel="stylesheet" />
 {{-- <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}" /> --}}
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet" />
 
 <script src="{{ asset('assets/component.min.js') }}"></script>
 <script src="{{ asset('assets/alpine.min.js') }}" defer></script>
@@ -16,4 +17,3 @@
 
 @vite('resources/js/app.js')
 @vite('resources/css/app.css')
-@vite('node_modules/font-awesome/css/font-awesome.min.css')
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                "font-awesome/css/font-awesome.min.css",
                 "resources/css/app.css",
                 "resources/js/app.js",
             ],
