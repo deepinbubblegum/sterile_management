be2823ddf7e6d28c692d9b1c38884b9c46f65245
deepinbubblegum <moondeepin@gmail.com>
2022-09-29 22:49:13 +0700

    update

--- resources/views/createOders.blade.php
+++ resources/views/createOders.blade.php
@@ -151,6 +151,7 @@
                                             </button>
 
                                             <button href="http://localhost/oders/create" class="px-4 mr-2 py-2 text-base text-white rounded-md bg-primary inline-flex items-center hover:bg-primary-dark focus:outline-none focus:ring focus:ring-primary focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark">
+                                                <i class="fa-solid fa-plus fa-xl mr-2"></i> 
                                                 เพิ่มรายการอุปกรณ์
                                             </button>
                                         </div>
