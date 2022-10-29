45bc0d5add3595906de2d1018d6df99cf08ecae9
deepinbubblegum <deepin.anorider@gmail.com>
2022-10-30 03:03:53 +0700

    update

--- app/Http/Controllers/Reports_Controller.php
+++ app/Http/Controllers/Reports_Controller.php
@@ -1,14 +1,13 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Exports\ReportsWashingCycle;
+
 use Exception;
 use Illuminate\Support\Facades\Cookie;
 use Carbon\Carbon;
@@ -21,6 +20,8 @@ class Reports_Controller extends BaseController
 {
     public function ExportExcelWashingCycle(Request $request)
     {
-        return Excel::download(new WashingCycleExport($request->input('id')), 'washing_cycle.xlsx');
+        $report = new ReportsWashingCycle();
+        $reportFrame = $report->collection('aa');
+        return Excel::download($reportFrame, 'washing_cycle.xlsx');
     }
 }
