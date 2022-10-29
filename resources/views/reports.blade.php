eca635b684e798e557e0b3987c877e83f94c4806
deepinbubblegum <deepin.anorider@gmail.com>
2022-10-30 04:25:07 +0700

    add link

--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -80,11 +80,11 @@
                                         placeholder="Select date end">
                                 </div>
                             </div>
-                            <button type="button"
-                                class="text-white w-full my-2 bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
+                            <a href="/reports/export/excel/washing" type="button" 
+                                class="cursor-pointer text-white w-full my-2 bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
                                 <i class="fa-solid fa-file-excel mr-2 -ml-1 fa-xl"></i>
                                 Export to Excel (.xlsx)
-                            </button>
+                            </a>
                     </div>
                     <div class="mx-auto mt-2 w-full rounded-md bg-white dark:bg-darker dark:text-light shadow-sm p-4 leading-6">
                         Reports Sterlie Cycle
