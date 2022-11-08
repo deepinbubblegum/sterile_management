96ce329ec262c52164b1dda3c7c73d88952be1ed
deepinbubblegum <deepin.anorider@gmail.com>
2022-11-08 22:01:48 +0700

    update edit permit

--- public/assets/js/orderPage.js
+++ public/assets/js/orderPage.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
                     <span
                         class="text-gray-700 dark:text-light px-1 py-2 flex items-center">
                         <a type="button" value="${element.Order_id}" href="/orders/edit/${element.Order_id}"
-                            class="edit_order mr-1 w-10 h-10 px-2 py-2 text-base text-white rounded-md bg-primary inline-flex items-center hover:bg-primary-dark focus:outline-none focus:ring focus:ring-primary focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark">
+                            class="edit_order ${(element.StatusApprove == 1 ? (permitt['Force Edit Orders'] == 1 ? '' : 'invisible') : '')} mr-1 w-10 h-10 px-2 py-2 text-base text-white rounded-md bg-primary inline-flex items-center hover:bg-primary-dark focus:outline-none focus:ring focus:ring-primary focus:ring-offset-1 focus:ring-offset-white dark:focus:ring-offset-dark">
                             <i class="fa-regular fa-pen-to-square fa-xl mx-auto"></i>
                         </a>
                         <a type="button" value="${element.Order_id}" href="/orders/pdf?orderid=${element.Order_id}" target="_blank"
