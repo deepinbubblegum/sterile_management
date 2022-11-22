4ff8234dbc20f73911d1941595e78632ef2bfec8
deepinbubblegum <moondeepin@gmail.com>
2022-11-22 21:07:09 +0700

    update - reports stock

--- public/assets/js/reports.js
+++ public/assets/js/reports.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('#departments_list').select2();
     $('#departments_list_Detail').select2();
+    $('#departments_list_delivery').select2();
     const url_path = window.location.pathname;
     $('.excel_xlsx_order').click(function (e) { 
         e.preventDefault();
@@ -32,13 +33,16 @@ $(document).ready(function () {
     function setCustomerslist(data){
         $('#customers_list').empty();
         $('#customers_list_Detail').empty();
+        $('#customers_list_delivery').empty();
         data.forEach((element) => {
             const html = `<option value="${element.Customer_id}">${element.Customer_name}</option>`;
            $('#customers_list').append(html);
            $('#customers_list_Detail').append(html);
+           $('#customers_list_delivery').append(html);
         });
         $('#customers_list').change();
         $('#customers_list_Detail').change();
+        $('#customers_list_delivery').change();
     }
 
     function getCustomer() {
@@ -133,5 +137,65 @@ $(document).ready(function () {
         console.log(date_Process_start);
         console.log(date_Process_end);
         window.open(`${url_path}/export/excel/${Customer_id}/process/${only_Approve}/${department_id}/between/${date_Process_start}/and/${date_Process_end}`, '_blank');
+    });
+
+    function setDepartmentslist_delivery(data){
+        $('#departments_list_delivery').empty();
+        const html = `<option value="ALL">เลือกทั้งหมด</option>`;
+        $('#departments_list_delivery').append(html);
+        data.forEach((element) => {
+            const html = `<option value="${element.Department_id}">${element.Department_name}</option>`;
+           $('#departments_list_delivery').append(html);
+        });
+        $('#departments_list_delivery').change();
+    }
+
+    $('#customers_list_delivery').change(function (e) { 
+        e.preventDefault();
+        Customer_id = $(this).val();
+        $.ajax({
+            type: "GET",
+            url: "/reports/getlistdepartments",
+            data: {
+                Customer_id: Customer_id
+            },
+            dataType: "json",
+            success: function (response) {
+                setDepartmentslist_delivery(response);
+            }
+        });
+    });
+
+    $('.excel_xlsx_delivery').click(function (e) {
+        e.preventDefault();
+        date_delivery_start = $('.date_Process_delivery_start').val();
+        date_delivery_end = $('.date_Process_delivery_end').val();
+        Customer_id = $('#customers_list_delivery').val();
+        department_id = $('#departments_list_delivery').val();
+
+        if (date_delivery_start == '' || date_delivery_end == '') {
+            alert('Please select date range');
+            return false;
+        }
+        data_delivery_start_earray = date_delivery_start.split("/");
+        data_delivery_end_earray = date_delivery_end.split("/");
+        date_delivery_start = data_delivery_start_earray[2] + '-' + data_delivery_start_earray[1] + '-' + data_delivery_start_earray[0];
+        date_delivery_end = data_delivery_end_earray[2] + '-' + data_delivery_end_earray[1] + '-' + data_delivery_end_earray[0];
+        console.log(date_delivery_start);
+        console.log(date_delivery_end);
+
+        filter_status = null
+        if($('#radio_all_status').is(':checked')){
+            filter_status = 'ALL';
+        }else if($('#radio_stock').is(':checked')){
+            filter_status = 'Stock';
+        }else if($('#radio_deliver').is(':checked')){
+            filter_status = 'Deliver';
+        }else if($('#radio_backlog').is(':checked')){
+            filter_status = 'Backlog';
+        }
+        console.log(filter_status);
+
+        window.open(`${url_path}/export/excel/${Customer_id}/stock/${filter_status}/${department_id}/between/${date_delivery_start}/and/${date_delivery_end}`, '_blank');
     });
 });
